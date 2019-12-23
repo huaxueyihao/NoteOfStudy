06830ec9298877e359b0e659c404e4483ffd59f4
huaxueyihao <837403116@qq.com>
2019-12-23 15:38:14 +0800

    chapter04

--- A07.Thread/B01.MultiThreadPractiseDesignPatterns/src/main/java/com/thread/study/chapter04/AlarmInfo.java
+++ A07.Thread/B01.MultiThreadPractiseDesignPatterns/src/main/java/com/thread/study/chapter04/AlarmInfo.java
@@ -1,4 +1,41 @@
 package com.thread.study.chapter04;
 
+import com.thread.study.chapter05.AlarmType;
+
 public class AlarmInfo {
+
+    public String id;
+
+    public AlarmType type;
+
+    public String extraInfo;
+
+    public AlarmInfo(String id, AlarmType type) {
+        this.id = id;
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public AlarmType getType() {
+        return type;
+    }
+
+    public void setType(AlarmType type) {
+        this.type = type;
+    }
+
+    public String getExtraInfo() {
+        return extraInfo;
+    }
+
+    public void setExtraInfo(String extraInfo) {
+        this.extraInfo = extraInfo;
+    }
 }
