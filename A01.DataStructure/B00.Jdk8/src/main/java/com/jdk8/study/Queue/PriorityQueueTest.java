d167c93b72d0ad143da4c7970fd22f7311e137f5
huaxueyihao <837403116@qq.com>
2019-12-25 20:46:37 +0800

    PriorityQueue

---
+++ A01.DataStructure/B00.Jdk8/src/main/java/com/jdk8/study/Queue/PriorityQueueTest.java
@@ -0,0 +1,24 @@
+package com.jdk8.study.Queue;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueueTest {
+
+    public static void main(String[] args) {
+
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
+        queue.add(3);
+        queue.add(5);
+        queue.add(10);
+        queue.add(7);
+        queue.add(9);
+        queue.add(15);
+        queue.add(11);
+        queue.add(13);
+        queue.add(20);
+        queue.add(12);
+
+    }
+
+
+}
