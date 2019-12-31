3724143824e8c4940158d21b4304d1abd1bab955
huaxueyihao <837403116@qq.com>
2019-12-31 21:25:54 +0800

    Nio buffer

---
+++ A04.Netty/B02.Netty_lecture/src/main/java/com/shengsiyuan/nio/NioTest10.java
@@ -0,0 +1,26 @@
+package com.shengsiyuan.nio;
+
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+
+public class NioTest10 {
+
+    public static void main(String[] args) throws Exception {
+        RandomAccessFile randomAccessFile = new RandomAccessFile("NioTest10.txt", "rw");
+
+        FileChannel fileChannel = randomAccessFile.getChannel();
+
+        FileLock lock = fileChannel.lock(3, 6, true);
+        System.out.println("valid: " + lock.isValid());
+        System.out.println("lock type : " + lock.isShared());
+
+        lock.release();
+
+        randomAccessFile.close();
+
+
+    }
+
+}
