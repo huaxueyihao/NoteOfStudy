275e47a5661c043f935a608358b161bd8f3f9ebe
huaxueyihao <837403116@qq.com>
2020-01-03 11:53:04 +0800

    chapter10 template method pattern

---
+++ A03.DesignPattern/B01.TheZenOfDesignPatterns/src/main/java/com/book/study/zen/chapter10/HummerH1Model.java
@@ -0,0 +1,25 @@
+package com.book.study.zen.chapter10;
+
+public class HummerH1Model extends HummerModel {
+    @Override
+    public void start() {
+        System.out.println("悍马H1启动.....");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("悍马H1停车....");
+    }
+
+    @Override
+    public void alarm() {
+        System.out.println("悍马H1鸣笛.....");
+
+    }
+
+    @Override
+    public void engineBoom() {
+        System.out.println("悍马H1引擎声音是这样的....");
+
+    }
+}
