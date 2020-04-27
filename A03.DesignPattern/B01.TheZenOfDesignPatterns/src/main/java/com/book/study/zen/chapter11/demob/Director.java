f170a430827ad120c9f6eddbf9a2129b62e1ed33
huaxueyihao <837403116@qq.com>
2020-04-27 11:48:18 +0800

    demob

---
+++ A03.DesignPattern/B01.TheZenOfDesignPatterns/src/main/java/com/book/study/zen/chapter11/demob/Director.java
@@ -0,0 +1,18 @@
+package com.book.study.zen.chapter11.demob;
+
+public class Director {
+
+    private Builder builder = new ConcreteProduct();
+
+    // 构建不同的产品
+    public Product getProduct() {
+        builder.setPart();
+
+        /**
+         * 设置不同的零件，产生不同的产品
+         */
+        return builder.buildProduct();
+    }
+
+
+}
---
+++ A03.DesignPattern/B01.TheZenOfDesignPatterns/src/main/java/com/book/study/zen/chapter11/demob/Product.java
@@ -0,0 +1,10 @@
+package com.book.study.zen.chapter11.demob;
+
+public class Product {
+
+    public void doSomething(){
+        // 独立业务处理
+    }
+
+
+}
