b13700179dd3bc6cfd424b56cfe5cf0198c9f3c7
huaxueyihao <837403116@qq.com>
2020-04-28 08:41:44 +0800

    IGamePlayer

---
+++ A03.DesignPattern/B01.TheZenOfDesignPatterns/src/main/java/com/book/study/zen/chapter12/IGamePlayer.java
@@ -0,0 +1,16 @@
+package com.book.study.zen.chapter12;
+
+public interface IGamePlayer {
+
+    // 登陆游戏
+    public void login(String user,String password);
+
+    // 杀怪，网络游戏的主要特色
+    public void killBoss();
+
+    // 升级
+    public void upgrade();
+
+
+
+}
