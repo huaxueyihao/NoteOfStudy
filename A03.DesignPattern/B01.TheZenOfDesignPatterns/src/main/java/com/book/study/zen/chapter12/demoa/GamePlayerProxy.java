64f8ec4862e34c2713d8aff4d00b830d61aabb26
huaxueyihao <837403116@qq.com>
2020-04-28 08:38:52 +0800

    chapter12:demoa

---
+++ A03.DesignPattern/B01.TheZenOfDesignPatterns/src/main/java/com/book/study/zen/chapter12/demoa/GamePlayerProxy.java
@@ -0,0 +1,28 @@
+package com.book.study.zen.chapter12.demoa;
+
+import com.book.study.zen.chapter12.IGamePlayer;
+
+public class GamePlayerProxy implements IGamePlayer {
+
+
+    private IGamePlayer gamePlayer = null;
+
+    public GamePlayerProxy(IGamePlayer gamePlayer) {
+        this.gamePlayer = gamePlayer;
+    }
+
+    @Override
+    public void login(String user, String password) {
+        this.gamePlayer.login(user,password);
+    }
+
+    @Override
+    public void killBoss() {
+        this.gamePlayer.killBoss();
+    }
+
+    @Override
+    public void upgrade() {
+        this.gamePlayer.upgrade();
+    }
+}
