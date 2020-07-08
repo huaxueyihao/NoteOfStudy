1640eacd45b2a9049ab799029d6d02097645cbac
huaxueyihao <837403116@qq.com>
2020-07-09 07:48:27 +0800

    payment8001

---
+++ A15.SpringCloud/B02.GuiGuCloud/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -0,0 +1,13 @@
+package com.atguigu.springcloud.service;
+
+import com.atguigu.springcloud.entities.Payment;
+
+public interface PaymentService {
+
+     int create(Payment payment);
+
+
+     Payment getPaymentById(Long id);
+
+
+}
