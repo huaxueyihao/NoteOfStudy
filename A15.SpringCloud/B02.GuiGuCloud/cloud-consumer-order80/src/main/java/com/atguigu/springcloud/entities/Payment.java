cda0390f3d823f936ca3b3ebffbdeb107a095c82
huaxueyihao <837403116@qq.com>
2020-07-09 22:55:48 +0800

    order80

---
+++ A15.SpringCloud/B02.GuiGuCloud/cloud-consumer-order80/src/main/java/com/atguigu/springcloud/entities/Payment.java
@@ -0,0 +1,23 @@
+package com.atguigu.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment {
+
+    private Long id;
+
+    private String serial;
+
+
+}
