534e2dda29dbdd16a44fb7da7f0d2005167e9469
huaxueyihao <837403116@qq.com>
2020-07-12 13:20:36 +0800

    payment8004 zk

---
+++ A15.SpringCloud/B02.GuiGuCloud/cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,27 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.UUID;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @PostMapping(value = "/payment/create")
+    public String payment() {
+        return "springcloud with zookeeper: " +serverPort+"\t"+ UUID.randomUUID().toString();
+    }
+
+
+
+}
