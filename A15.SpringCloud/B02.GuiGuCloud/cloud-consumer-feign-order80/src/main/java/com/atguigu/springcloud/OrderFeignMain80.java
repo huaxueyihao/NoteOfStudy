09ee22784a7e7182d2a7347d40cc8ae9e4a246ec
huaxueyihao <837403116@qq.com>
2020-07-15 20:46:49 +0800

    order80 feign

---
+++ A15.SpringCloud/B02.GuiGuCloud/cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderFeignMain80.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableFeignClients
+public class OrderFeignMain80 {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(OrderFeignMain80.class,args);
+
+    }
+
+
+}
