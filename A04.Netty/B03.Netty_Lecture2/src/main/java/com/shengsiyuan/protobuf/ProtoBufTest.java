2b7085b4d1f3ad4591e4f15fa32ec31e8010f55c
huaxueyihao <837403116@qq.com>
2020-09-23 07:36:40 +0800

    protobuf

---
+++ A04.Netty/B03.Netty_Lecture2/src/main/java/com/shengsiyuan/protobuf/ProtoBufTest.java
@@ -0,0 +1,20 @@
+package com.shengsiyuan.protobuf;
+
+
+public class ProtoBufTest {
+
+    public static void main(String[] args) throws Exception {
+
+        DataInfo.Student student = DataInfo.Student.newBuilder().setName("张三").setAge(20).setAddress("北京").build();
+        byte[] student2ByteArray = student.toByteArray();
+
+
+        DataInfo.Student student2 = DataInfo.Student.parseFrom(student2ByteArray);
+
+        System.out.println(student2);
+        System.out.println(student2.getName()+" "+student2.getAge()+" "+student2.getAddress());
+
+    }
+
+
+}
