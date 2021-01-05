ebf1fcdc291e0a55a06bc715c064bef94961aba7
huaxueyihao <837403116@qq.com>
2021-01-05 22:07:46 +0800

    learnvuerouter

--- A18_Vue/B01_VueOfViedo/LearnVuejs06/learnvuerouter/src/App.vue
+++ A18_Vue/B01_VueOfViedo/LearnVuejs06/learnvuerouter/src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="app">
     <div>
-      <router-link to="/home">首页</router-link>
-      <router-link to="/about">关于</router-link>
+      <!-- <router-link to="/home" tag="button">首页</router-link>
+      <router-link to="/about" tag="button">关于</router-link> -->
+
+      <button @click="homeClick">首页</button>
+      <button @click="aboutClick">关于</button>
+
       <router-view></router-view>
     </div>
   </div>
@@ -12,6 +16,14 @@
 
 export default {
   name: 'App',
+  method: {
+    homeClick(){
+      this.$router.replace('/home');
+    },
+    aboutClick(){
+      this.$router.replace('/about');
+    }
+  },
   components: {
 
   }
