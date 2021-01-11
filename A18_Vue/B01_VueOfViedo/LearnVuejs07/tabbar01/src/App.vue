168bffeaa30444647f4fb70ea133276166e59e25
huaxueyihao <837403116@qq.com>
2021-01-11 22:30:57 +0800

    tabbar01 init

---
+++ A18_Vue/B01_VueOfViedo/LearnVuejs07/tabbar01/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <tab-bar>
+      <tab-bar-item>
+        <img src="" alt=""/>
+        <div>首页</div>
+      </tab-bar-item>
+
+      <tab-bar-item>
+        <img src="" alt=""/>
+        <div>首页</div>
+      </tab-bar-item>
+
+      <tab-bar-item>
+        <img src="" alt=""/>
+        <div>首页</div>
+      </tab-bar-item>
+
+      <tab-bar-item>
+        <img src="" alt=""/>
+        <div>首页</div>
+      </tab-bar-item>
+
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+
+import TabBar from './components/tabbar/TabBar.vue'
+import TabBarItem from './components/tabbar/TabBarItem.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    TabBar,TabBarItem
+  }
+}
+</script>
+
+<style>
+  @import "./assets/css/base.css";
+
+
+</style>
---
+++ A18_Vue/B01_VueOfViedo/LearnVuejs07/tabbar01/src/components/tabbar/TabBar.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id="tab-bar">
+      <div class="tab-bar-item"> 
+          <img src="" alt="">
+          首页1
+      </div>
+      <div class="tab-bar-item"> 
+          <img src="" alt="">
+          首页2
+      </div>
+      <div class="tab-bar-item">
+          <img src="" alt="">
+          首页3
+      </div>
+      <div class="tab-bar-item">
+          <img src="" alt="">
+          首页4
+      </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "TabBar"
+}
+</script>
+
+<style  scoped>
+
+  #tab-bar {
+    display: flex;
+    background-color: #f6f6f6;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+
+    box-shadow: 0px -3px 1px rgba(100, 100, 100, .1);
+  }
+
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+  }
+</style>
