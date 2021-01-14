9e398f4d416d7aa1e7325f59fccdb4e220617047
huaxueyihao <837403116@qq.com>
2021-01-14 22:02:02 +0800

    vuex

---
+++ A18_Vue/B01_VueOfViedo/LearnVuejs08/learnvuex/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h2>-------------App内容--------------</h2>
+    <h2>{{ $store.state.counter }}</h2>
+    <button @click="$store.state.counter++">+</button>
+    <button @click="$store.state.counter--">-</button>
+
+    <h2>-------------hello Vuex内容--------------</h2>
+    <hello-vuex />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import HelloVuex from "./components/HelloVuex.vue";
+
+export default {
+  components: { HelloVuex },
+  name: "App",
+  components: {
+    HelloVuex,
+  },
+};
+</script>
+
+<style>
+</style>
