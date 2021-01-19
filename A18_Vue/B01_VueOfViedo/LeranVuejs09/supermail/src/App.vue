2fcc7be2d96398f46f86d48a4f4cab0af39d6ccb
huaxueyihao <837403116@qq.com>
2021-01-19 08:02:29 +0800

    supermail tabbar

--- A18_Vue/B01_VueOfViedo/LeranVuejs09/supermail/src/App.vue
+++ A18_Vue/B01_VueOfViedo/LeranVuejs09/supermail/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
-
+      <router-view/>
+      <main-tab-bar/>
   </div>
 </template>
 
 <script>
+import MainTabBar from './components/content/mainTabbar/MainTabBar.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainTabBar
   }
 }
 </script>
